02dfea040b44a2526748c86723cd70ac2cc1875e
glyzinieh <glyzinie.h@gmail.com>
2022-05-17 23:18:20 +0900

    スプレッドシートを開く

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # share-neppuri
 コンビニのネップリを共有するためのサイト
+
+# 環境変数について
+.envに以下の内容を記述します
+SHEET_PROJECT_ID=(project_id)
+SHEET_PRIVATE_KEY_ID=(private_key_id)
+SHEET_PRIVATE_KEY=(private_key)
+SHEET_CLIENT_EMAIL=(client_email)
+SHEET_CLIENT_ID=(client_id)
+SHEET_CLIENT_X509_CERT_URL=(client_x509_cert_url)
+
+SHEET_DATABASE_KEY=(シートID)
--- app.py
+++ app.py
@@ -1,6 +1,42 @@
-from flask import Flask
+import os
+from os.path import dirname, join
+
+import gspread
+from dotenv import load_dotenv
+from flask import Flask, render_template
+from google.oauth2.service_account import Credentials
+
+# ローカル環境で環境編集を取得
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Googleスプレッドシートからデータベースを取得
+scope = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+
+credential = {
+    "type": "service_account",
+    "project_id": os.environ['SHEET_PROJECT_ID'],
+    "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+    "private_key": os.environ['SHEET_PRIVATE_KEY'],
+    "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+    "client_id": os.environ['SHEET_CLIENT_ID'],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+    }
+credentials = Credentials.from_service_account_info(credential,scopes=scope)
+# ログイン
+gc = gspread.authorize(credentials)
+
+# データベースのシートを開く
+database_sheet = gc.open_by_key(os.environ['SHEET_DATABASE_KEY'])
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(port=8080,debug=True)
