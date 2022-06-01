fa63c0fedb30e38d2b48aa3458b1c9138d0e2e05
glyzinieh <glyzinie.h@gmail.com>
2022-06-01 23:41:20 +0900

    登録処理を追加

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from os.path import dirname, join
 
 import client
 import gspread
-import requests
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sitemap import Sitemap
 from google.oauth2.service_account import Credentials
+from werkzeug.utils import secure_filename
 
 # ローカル環境で環境編集を取得
 dotenv_path = join(dirname(__file__), '.env')
@@ -31,14 +31,19 @@ credential = {
 credentials = Credentials.from_service_account_info(credential,scopes=scope)
 # ログイン
 gc = gspread.authorize(credentials)
+# シートを開く
+wb = gc.open_by_key(os.environ['SHEET_DATABASE_KEY'])
+ws = wb.worksheet("Main")
 
-# データベースのシートを開く
-database_sheet = gc.open_by_key(os.environ['SHEET_DATABASE_KEY'])
-
-http_data = client.file_send(os.environ['DB_URL'],os.environ['DB_TOKEN'])
+file_send = client.file_send(os.environ['DB_URL'],os.environ['DB_TOKEN'])
 
 app = Flask(__name__)
 ext = Sitemap(app=app)
+
+ALLOWED_EXTENSIONS = {'png','jfif','pjpeg','jpeg','pjp','jpg'}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.get('/')
 def index():
@@ -70,7 +75,50 @@ def record():
 
 @app.post('/record/thanks/')
 def record_thanks():
-    return render_template('record_thanks.html')
+    # fileがない場合
+    if 'image' not in request.files:
+        return render_template(
+            'record_thanks.html',
+            status='登録できませんでした。ファイルを送信してください。'
+            )
+    file = request.files['image']
+
+    # filenameが空の場合
+    if file.filename == '':
+        return render_template(
+            'record_thanks.html',
+            status='登録できませんでした。ファイルの名前を指定してください。'
+            )
+
+    if file and allowed_file(file.filename):
+        fileext = os.path.splitext(secure_filename(file.filename))[1]
+    else:
+        return render_template(
+            'record_thanks.html',
+            status='登録できませんでした。PNG,JPEGのみ送信できます。'
+            )
+
+    id = str(ws.row_count + 1).zfill(5)
+    filename = id + fileext
+    answer = request.form
+    save_data = [
+        id,
+        answer["email"],
+        answer["name"],
+        answer["title"],
+        filename,
+        answer["date"],
+        answer["note"],
+        answer["no_seven"],
+        answer["no_family"]
+    ]
+    
+    ws.append_row(save_data)
+    file_send.write(filename,file)
+    return render_template(
+            'record_thanks.html',
+            status='ご登録ありがとうございます。'
+            )
 
 @app.get('/privacy-policy/')
 def privacy():
